3a9eb18899b45735a3a9324f20480be068675e40
ezh247467 <e8huang@ucsd.edu>
2024-05-02 15:52:08 -0700

    Completed writing the unit tests

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Lab 5 - Starter
 Ethan Huang
+
+### Check your Understanding
+1. I would not use a unit test for this feature because this messaging feature is culmination of multiple individual parts of the code that each need to be tested.
+2. "Max message length" feature is something that can be unit tested because it is an individual part of the code that can be tested without effect on the other parts.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,72 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+// PhoneNumber
+test('Check phone number type 1', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('Check phone number type 2', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('Check non 10 values', () => {
+  expect(isPhoneNumber('911')).toBe(false);
+});
+test('Check no dashes', () => {
+  expect(isPhoneNumber('9119114569')).toBe(false);
+});
+
+// Email
+test('Check conventional email', () => {
+  expect(isEmail('test@gmail.com')).toBe(true);
+});
+test('Check ucsd email', () => {
+  expect(isEmail('test@ucsd.edu')).toBe(true);
+});
+test('Check school district email (non-working due to Regex limits)', () => {
+  expect(isEmail('test@cnusd.k12.edu')).toBe(false);
+});
+test('Check no @', () => {
+  expect(isEmail('testucsd.edu')).toBe(false);
+});
+
+// StrongPassword
+test('Check all lowercase', () => {
+  expect(isStrongPassword('testucsdedu')).toBe(true);
+});
+test('Check short', () => {
+  expect(isStrongPassword('test')).toBe(true);
+});
+test('Check too long', () => {
+  expect(isStrongPassword('testucsdeduqwert3456')).toBe(false);
+});
+test('Check too short', () => {
+  expect(isStrongPassword('tes')).toBe(false);
+});
+
+// Date
+test('Check single digit month/day', () => {
+  expect(isDate('1/1/2024')).toBe(true);
+});
+test('Check double digit month/day', () => {
+  expect(isDate('10/10/2024')).toBe(true);
+});
+test('Check double digit year', () => {
+  expect(isDate('10/10/24')).toBe(false);
+});
+test('Check dash instead of slash', () => {
+  expect(isDate('10-10-2024')).toBe(false);
+});
+
+// Hex Color
+test('Check 3 digit code', () => {
+  expect(isHexColor('#000')).toBe(true);
+});
+test('Check 6 digit code', () => {
+  expect(isHexColor('#FFFFFF')).toBe(true);
+});
+test('Check 7 digit code', () => {
+  expect(isHexColor('#FFFFFFF')).toBe(false);
+});
+test('Check 2 digit code', () => {
+  expect(isHexColor('#FF')).toBe(false);
+});
