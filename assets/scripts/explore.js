c1eb1b3cee91f19fa4ceee890ff37a48542c1a4d
ezh247467 <e8huang@ucsd.edu>
2024-05-01 23:40:54 -0700

    Completed explore part of lab

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,38 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var voices = [];
+  var button = document.querySelector("button");
+  const voiceSelect = document.getElementById("voice-select");
+  window.speechSynthesis.onvoiceschanged = function() {
+    voices = window.speechSynthesis.getVoices();
+    
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  };
+
+  button.addEventListener('click', function() {
+    var textInput = document.getElementById("text-to-speak");
+    var speech = new SpeechSynthesisUtterance();
+    speech.text = textInput.value;
+    var voice = voices.find(function(voice) {return `${voice.name} (${voice.lang})` == voiceSelect.value})
+    let face = document.querySelector("img");
+    speech.voice = voice;
+
+    speech.onstart = function() {
+      face.src = "assets/images/smiling-open.png";
+    };
+
+    speech.onend = function() {
+      face.src = "assets/images/smiling.png";
+    };
+
+    window.speechSynthesis.speak(speech);
+  })
 }
